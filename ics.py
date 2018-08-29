e4bf03802a6449649298ae7bab16e157b8bd7cc5
Tantalus <jsl_713@live.com>
2018-08-30 07:45:50 +0800

    ics handling

---
+++ ics.py
@@ -0,0 +1,30 @@
+import vobject
+
+
+class ICSCreator:
+    def __init__(self):
+        self._cal = vobject.iCalendar()
+    
+    def add_event(self, title, dtstart, dtend, rrule=None):
+        vevent = self._cal.add('vevent')
+        vevent.add('summary').value = title
+        vevent.add('dtstart').value = dtstart
+        vevent.add('dtend').value = dtend
+        if rrule:
+            vevent.add('rrule').value = rrule
+    
+    def serialize(self, filename):
+        text = self._cal.serialize()
+        if filename:
+            with open(filename, 'w', encoding='utf-8', newline='') as f:
+                f.write(text)
+        return text
+    
+    @classmethod
+    def rrule(cls, interval=1, count=16, freq='WEEKLY'):
+        data = {
+            'FREQ': freq,
+            'INTERVAL': interval,
+            'COUNT': count
+        }
+        return ';'.join('%s=%s' % kv for kv in data.items())
