4bd169be4caccc9238ffd1f65ca03e99beeef589
Taraxacum <john980118@163.com>
2018-08-31 14:33:18 +0800

    move classroom information from SUMMARY to LOCATION

--- ics.py
+++ ics.py
@@ -4,22 +4,24 @@ import vobject
 class ICSCreator:
     def __init__(self):
         self._cal = vobject.iCalendar()
-    
-    def add_event(self, title, dtstart, dtend, rrule=None):
+
+    def add_event(self, title, dtstart, dtend, location=None, rrule=None):
         vevent = self._cal.add('vevent')
         vevent.add('summary').value = title
         vevent.add('dtstart').value = dtstart
         vevent.add('dtend').value = dtend
+        if location:
+            vevent.add('location').value = location
         if rrule:
             vevent.add('rrule').value = rrule
-    
+
     def serialize(self, filename=None):
         text = self._cal.serialize()
         if filename:
             with open(filename, 'w', encoding='utf-8', newline='') as f:
                 f.write(text)
         return text
-    
+
     @classmethod
     def rrule(cls, interval=1, count=16, freq='WEEKLY'):
         data = {
