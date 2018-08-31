d4c20923809c8946aa6dca7bb09e4fb12b06dcfe
Taraxacum <john980118@163.com>
2018-08-31 17:01:28 +0800

    timezone supported

--- ics.py
+++ ics.py
@@ -1,15 +1,29 @@
 import vobject
+import pytz
+
+TIMEZONE = '''
+BEGIN:VTIMEZONE
+TZID:Asia/Shanghai
+BEGIN:STANDARD
+DTSTART:16010101T000000
+TZOFFSETFROM:+0800
+TZOFFSETTO:+0800
+END:STANDARD
+END:VTIMEZONE
+'''
 
 
 class ICSCreator:
     def __init__(self):
         self._cal = vobject.iCalendar()
+        self._cal.add(vobject.readOne(TIMEZONE, validate=True))
+        self.tz = pytz.timezone('Asia/Shanghai')
 
     def add_event(self, title, dtstart, dtend, location=None, rrule=None):
         vevent = self._cal.add('vevent')
         vevent.add('summary').value = title
-        vevent.add('dtstart').value = dtstart
-        vevent.add('dtend').value = dtend
+        vevent.add('dtstart').value = self.tz.localize(dtstart)
+        vevent.add('dtend').value = self.tz.localize(dtend)
         if location:
             vevent.add('location').value = location
         if rrule:
